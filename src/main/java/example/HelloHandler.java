1056b0ae9707c5fff777d06e6e0c0ad71fa569c1
mbimbij <joseph.mbimbi@gmail.com>
2021-03-12 09:16:06 +0100

    v5

--- src/main/java/example/HelloHandler.java
+++ src/main/java/example/HelloHandler.java
@@ -6,6 +6,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class HelloHandler implements RequestHandler<Object,HelloResponse> {
   @Override
   public HelloResponse handleRequest(Object helloRequest, Context context) {
-    return new HelloResponse("hello from my-app-autonome - v4");
+    return new HelloResponse("hello from my-app-autonome - v5");
   }
 }
